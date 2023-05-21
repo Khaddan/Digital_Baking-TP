d8172544016ff3ca6479bceff6601f00f0a05ace
khaddan <khaddanzakaria7@gmail.com>
2023-05-21 15:01:05 +0100

    Back-end

---
+++ src/main/java/com/example/digital_banckingtp/DigitalBanckingTpApplication.java
@@ -0,0 +1,13 @@
+package com.example.digital_banckingtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBanckingTpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalBanckingTpApplication.class, args);
+	}
+
+}
